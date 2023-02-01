cb719244b09e33da49f850a47255ce9975f0738b
smitamisra <smitamisra74@gmail.com>
2023-01-31 21:40:45 -0600

    q19 and bonus 1

--- SM_sql_solo.sql
+++ SM_sql_solo.sql
@@ -64,6 +64,21 @@ GROUP BY name
 ORDER BY male_name_sum DESC;
 --"James"(total=5164280) is the most populat male name followed by "John"	5124817
 
+(SELECT name, gender, SUM(num_registered) AS total
+FROM names
+WHERE gender = 'M'
+GROUP BY gender, name
+ORDER BY total DESC
+LIMIT 1)
+UNION --using union
+(SELECT name, gender, SUM(num_registered) AS total
+FROM names
+WHERE gender = 'F'
+GROUP BY gender, name
+ORDER BY total DESC
+LIMIT 1);
+
+
 --9. What are the most popular boy and girl names of the first decade of the 2000s (2000 - 2009)?
 SELECT name, gender, SUM(num_registered) AS decade2000_name
 FROM names
@@ -132,6 +147,33 @@ SELECT
      WHERE gender='M')) * 100/
    CAST( COUNT(DISTINCT name) AS DOUBLE PRECISION)AS perc_unisex
 FROM names
+------
+(SELECT DISTINCT name
+FROM names
+WHERE gender = 'F')
+INTERSECT
+(SELECT DISTINCT name
+FROM names
+WHERE gender = 'M');
+
+
+
+-------
+SELECT ROUND(sum(unisex_names) * 100.0 /count(*), 2)
+FROM (
+SELECT name, CASE WHEN COUNT(DISTINCT(gender)) > 1  THEN 1
+END AS unisex_names
+FROM names
+GROUP BY name) as sub1
+-------
+SELECT COUNT(DISTINCT xyz.uni) AS unisex_tot, 
+	COUNT(DISTINCT(name)) AS all_names, 
+	ROUND((100.0*COUNT(DISTINCT xyz.uni)/COUNT(DISTINCT(name))), 1) AS perc
+
+FROM(SELECT name AS uni, COUNT(DISTINCT gender) AS count
+	FROM names
+	GROUP BY name 
+	HAVING COUNT(DISTINCT gender) > 1) AS xyz
 
 ----10.98  % are unisex names.
 --Q15. How many names have made an appearance in every single year since 1880?
@@ -140,9 +182,20 @@ SELECT name,
   FROM names
  WHERE year BETWEEN 1880 AND 2018
  GROUP BY  names.name
-HAVING COUNT(names.year) = 139;
+HAVING COUNT(DISTINCT names.year) = 139;
 -- ORDER BY name_count DESC;
---There are 136 names that have occured every year in the table.
+--There are 921 names that have occured every year in the table.
+--From Ajay
+SELECT COUNT(name)
+FROM
+(
+	SELECT DISTINCT name
+	FROM names
+	GROUP BY name
+	HAVING COUNT(DISTINCT year) = (SELECT COUNT(DISTINCT year) FROM names)
+) AS Sub1
+
+
 --Q16. How many names have only appeared in one year?
 SELECT name, 
 	COUNT(DISTINCT names.name) AS name_oneyearonly
@@ -153,6 +206,46 @@ HAVING COUNT(DISTINCT names.year) = 1;
 
 --tHERE ARE 21123 NAMES that only appear once. Looks a little too much need to revis the code
 
+--Q17 How many names only appeared in the 1950s?
+SELECT name
+FROM names
+GROUP BY name
+HAVING MIN(year)>=1950
+	AND MAX(year)<=1959;
+--661 names
+-------
+SELECT name
+FROM names
+GROUP BY name
+HAVING MIN(year) BETWEEN 1950 AND 1959
+	AND MAX(year) BETWEEN 1950 AND 1959;
+-------
+SELECT COUNT( DISTINCT Name)
+FROM names
+WHERE name NOT IN
+(
+	SELECT DISTINCT name
+	FROM names
+	WHERE year <  1950 OR year > 1959
+)
+AND Year BETWEEN 1950 and 1959
+--------------
+--Q18. How many names made their first appearance in the 2010s?
+SELECT name
+FROM names
+GROUP BY name
+HAVING MIN(year)>=2010
 
+--Q19. Find the names that have not be used in the longest.
+SELECT name, MAX(year)AS last_year_used
+FROM names
+GROUP BY name
+ORDER BY last_year_used DESC;
 
+-----do the bonus questions------------------
+--b1 Find the longest name contained in this dataset. What do you notice about the long names?
 
+SELECT *, CHAR_LENGTH(name) AS LengthOfName
+FROM names
+ORDER BY lengthofName DESC
+LIMIT 5;
